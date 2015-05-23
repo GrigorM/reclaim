48a8a907bdd0d7392723acfb807e715d7d72dd99
Grigor Malo <grigormalo@gmail.com>
2015-05-23 19:29:13 +0200

    Corrections

--- about/about_animation.js
+++ about/about_animation.js
@@ -1,8 +1,9 @@
 var div;
-
+var canvas;
 function setup() {
   // put setup code here
   canvas = createCanvas(screen.width, 300);
+  canvas.position(0, 0);
   background(50, 50, 150);
   div = createDiv("Testing, testing, 1 2 3");
   div.position(0, 50);
