42bc35635d20e8cc6930707b2cde641a772f01aa
GrigorM <g.malo@GMALO1.commprog.com>
2015-05-28 12:13:04 +0200

    About goes left

--- about/about_animation.js
+++ about/about_animation.js
@@ -2,11 +2,11 @@ var div;
 var canvas;
 function setup() {
   // put setup code here
-  canvas = createCanvas(screen.width, 300);
+  canvas = createCanvas(screen.width/2, screen.height);
   canvas.position(0, 0);
-  background(50, 50, 150);
+  background(255);
   div = createDiv("Testing, testing, 1 2 3");
-  div.position(0, 50);
+  div.position(screen.width/2, 50);
   div.class('customDiv');
 }
 
