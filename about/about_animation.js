35a85742a455fec124ad6f3265ba2a531284ca0f
GrigorM <g.malo@GMALO1.commprog.com>
2015-05-28 12:14:08 +0200

    Fixes

--- about/about_animation.js
+++ about/about_animation.js
@@ -3,11 +3,11 @@ var canvas;
 function setup() {
   // put setup code here
   canvas = createCanvas(screen.width/2, screen.height);
-  canvas.position(0, 0);
+  canvas.position(screen.width/2, 0);
   background(255);
-  div = createDiv("Testing, testing, 1 2 3");
-  div.position(screen.width/2, 50);
-  div.class('customDiv');
+  //div = createDiv("Testing, testing, 1 2 3");
+  //div.position();
+  //div.class('customDiv');
 }
 
 function draw() {
