8fea13e92bfc4028b1753554f39222fe014d3938
GrigorM <g.malo@GMALO1.commprog.com>
2015-05-28 12:21:03 +0200

    Fix it

--- about/about_animation.js
+++ about/about_animation.js
@@ -2,7 +2,7 @@ var div;
 var canvas;
 function setup() {
   // put setup code here
-  canvas = createCanvas(screen.width/2, screen.height);
+  canvas = createCanvas(screen.width/2, screen.height-200);
   canvas.position(screen.width/2, 0);
   background(255);
   //div = createDiv("Testing, testing, 1 2 3");
