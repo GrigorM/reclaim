f9941a4740420287fac74a7b85844ece2495b2a2
GrigorM <g.malo@GMALO1.commprog.com>
2015-05-29 14:39:29 +0200

    Hide/show posts and other changes

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,9 @@
 //custom jquery and javascript
 $(document).ready(function(){
 	$('.category-link > p').hide();
+	
+	$(".category-link").click(function(e) {
+		$(".article").hide();
+		$(".article-" + $(this).html()).show();
+	});
 });
